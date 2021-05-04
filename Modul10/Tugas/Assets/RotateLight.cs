b51cb54d20cf0332be4bb3a65b3ddfaf09c49997
rianbtk <mrprobojonegoro@gmail.com>
2021-05-04 14:40:39 +0700

    KM

---
+++ Modul10/Tugas/Assets/RotateLight.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+public class RotateLight : MonoBehaviour{
+    public float speed = -2.0f;
+    void Update(){
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    } 
+}
